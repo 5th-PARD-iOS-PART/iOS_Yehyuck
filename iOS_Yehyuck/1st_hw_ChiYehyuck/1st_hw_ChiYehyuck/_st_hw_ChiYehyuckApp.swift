b44c5422d048862bc6e3ecb6669a340b9d9ea94c
Yehyuck <22200843@handong.ac.kr>
2025-03-21 02:28:04 +0900

    Add files via upload

---
+++ iOS_Yehyuck/1st_hw_ChiYehyuck/1st_hw_ChiYehyuck/_st_hw_ChiYehyuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  _st_hw_ChiYehyuckApp.swift
+//  1st_hw_ChiYehyuck
+//
+//  Created by Yehyuck Chi on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct _st_hw_ChiYehyuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
